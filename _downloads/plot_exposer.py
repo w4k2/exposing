7eb4a7e565c12a1d0b68718ebc9559faeb08d694
w4k2 <pawel.ksieniewicz@pwr.edu.pl>
2018-03-13 22:39:26 +0000

    Pushing the docs for revision for branch: master, commit 5a22fda96759d461daff25d24d3f5f5ae16964c8

--- _downloads/plot_exposer.py
+++ _downloads/plot_exposer.py
@@ -14,13 +14,13 @@ from matplotlib import pyplot as plt
 from exposing import Exposer
 
 n_samples = 10000
-ds = make_circles(noise=0.2, factor=0.5, random_state=1, n_samples = n_samples)
-ds = make_moons(noise=0.3, random_state=0, n_samples = n_samples)
+ds = make_circles(noise=0.2, factor=0.5, random_state=1, n_samples=n_samples)
+ds = make_moons(noise=0.3, random_state=0, n_samples=n_samples)
 X, y = ds
 X_train, X_test, y_train, y_test = \
     train_test_split(X, y, test_size=.4, random_state=42)
 
-estimator = Exposer(a_steps=5, grain = 32)
+estimator = Exposer(a_steps=5, grain=32)
 estimator.fit(X_train, y_train)
 
 plt.imshow(1 - estimator.rgb())
