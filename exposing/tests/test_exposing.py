ee34b233689f0b659fe5efd42118046a7268ff02
Pawel Ksieniewicz <pawel@ksienie.com>
2018-03-14 23:56:33 +0100

    Location finding moved to separate method.

---
+++ exposing/tests/test_exposing.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy.testing import assert_almost_equal
+from sklearn.datasets import make_circles, make_moons
+from sklearn.model_selection import train_test_split
+import warnings
+
+from exposing import Exposer
+
+def test_demo():
+    n_samples = 1000
+    ds = make_circles(noise=0.2, factor=0.5, random_state=1, n_samples=n_samples)
+    ds = make_moons(noise=0.3, random_state=0, n_samples=n_samples)
+    X, y = ds
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.4, random_state=42)
+
+    estimator = Exposer()
+    estimator.fit(X_train, y_train)
+    score = estimator.score(X_test, y_test)
+    warnings.warn("Accuracy = %.3f" % score)
+
+
+def test_rgb():
+    n_samples = 1000
+    ds = make_circles(noise=0.2, factor=0.5, random_state=1, n_samples=n_samples)
+    ds = make_moons(noise=0.3, random_state=0, n_samples=n_samples)
+    X, y = ds
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.4, random_state=42)
+
+    estimator = Exposer()
+    estimator.fit(X_train, y_train)
+    rgb = estimator.rgb()
