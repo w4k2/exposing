c5e2ce72b8412cf3857b4e72e78f5d14b53816b8
w4k2 <pawel.ksieniewicz@pwr.edu.pl>
2018-03-16 13:46:24 +0000

    Pushing the docs for revision for branch: master, commit a24374dfc6fe9a0ef2cb7b6d84ce9c0852b19b29

--- _downloads/plot_ee.py
+++ _downloads/plot_ee.py
@@ -14,18 +14,18 @@ from sklearn.datasets import load_breast_cancer, load_iris
 
 import exposing
 
-ds = load_breast_cancer(return_X_y=True)
+ds = load_iris(return_X_y=True)
 X, y = ds
 X_train, X_test, y_train, y_test = \
     train_test_split(X, y, test_size=.4, random_state=42)
 
-estimator = exposing.EE(a_steps=5, grain=32, random_state=1, n_base=25)
+estimator = exposing.EE(a_steps=30, random_state=1, n_base=16)
 estimator.fit(X_train, y_train)
 
-fig, ax = plt.subplots(5, 5, figsize=(10, 10))
-for i in xrange(5):
-    for j in xrange(5):
-        index = i + j*5
+fig, ax = plt.subplots(4, 4, figsize=(10, 10))
+for i in xrange(4):
+    for j in xrange(4):
+        index = i + j*4
         ax[i, j].imshow(estimator.ensemble_[index].rgb())
         ax[i, j].set_title("%i - %s (%.2f)" % (
             index,
