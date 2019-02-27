6abbced18aa567ed45426ba915f3b56e7aeca028
Pawel Ksieniewicz <pawel@ksienie.com>
2019-02-27 15:19:16 +0100

    No streaming tests, case of EE for d=1 problem.

--- exposing/EE.py
+++ exposing/EE.py
@@ -11,12 +11,22 @@ from sklearn.utils.multiclass import _check_partial_fit_first_call
 from sklearn.utils.random import sample_without_replacement as swr
 from .Exposer import Exposer
 
-APPROACHES = ('brute', 'random', 'purified')
-FUSERS = ('equal', 'theta')
+APPROACHES = ("brute", "random", "purified")
+FUSERS = ("equal", "theta")
+
 
 class EE(BaseEstimator, ClassifierMixin):
-    def __init__(self, grain=16, a_steps=5, n_base=16, n_seek=32,
-                 approach='random', fuser='theta', random_state=0, focus = 2):
+    def __init__(
+        self,
+        grain=16,
+        a_steps=5,
+        n_base=16,
+        n_seek=32,
+        approach="random",
+        fuser="theta",
+        random_state=0,
+        focus=2,
+    ):
         self.grain = grain
         self.a_steps = a_steps
         self.n_base = n_base
@@ -33,7 +43,6 @@ class EE(BaseEstimator, ClassifierMixin):
             for e in self.ensemble_:
                 e.partial_fit(X, y)
 
-
     def fit(self, X, y):
         X, y = check_X_y(X, y)
         random_state = check_random_state(self.random_state)
@@ -46,27 +55,35 @@ class EE(BaseEstimator, ClassifierMixin):
 
         # Establish set of subspaces
         self.subspaces_ = None
-        if self.approach == 'brute' or self.approach == 'purified':
+        if self.approach == "brute" or self.approach == "purified":
             self.subspaces_ = list(itertools.combinations(range(self.n_features_), 2))
-        elif self.approach == 'random':
-            self.subspaces_ = [swr(self.n_features_, 2,
-                                   random_state=random_state)
-                               for i in range(self.n_base)]
+        elif self.approach == "random":
+            if self.n_features_ == 1:
+                self.subspaces_ = [(0, 0)]
+            else:
+                self.subspaces_ = [
+                    swr(self.n_features_, 2, random_state=random_state)
+                    for i in range(self.n_base)
+                ]
 
         # Compose ensemble
-        self.ensemble_ = [Exposer(grain=self.grain,
-                                  a_steps=self.a_steps,
-                                  focus = self.focus,
-                                  given_subspace=subspace)
-                          for subspace in self.subspaces_]
+        self.ensemble_ = [
+            Exposer(
+                grain=self.grain,
+                a_steps=self.a_steps,
+                focus=self.focus,
+                given_subspace=subspace,
+            )
+            for subspace in self.subspaces_
+        ]
 
         # Fit ensemble
         [clf.fit(X, y) for clf in self.ensemble_]
 
         # Cut purified ensemble
-        if self.approach == 'purified':
+        if self.approach == "purified":
             self.ensemble_.sort(key=lambda x: x.theta_, reverse=True)
-            self.ensemble_ = self.ensemble_[:self.n_base]
+            self.ensemble_ = self.ensemble_[: self.n_base]
 
         # Gather thetas
         self.thetas_ = np.array([clf.theta_ for clf in self.ensemble_])
@@ -104,66 +121,64 @@ class EE(BaseEstimator, ClassifierMixin):
         else:
             return None
 
-    def make_classification(self, n_samples = 1000, p = None):
+    def make_classification(self, n_samples=1000, p=None):
         # Get generation scheme
         pairs, units = self._prepare_generator()
 
         # Prepare labels
-        y = np.random.choice(self.classes_, n_samples, p = p)
+        y = np.random.choice(self.classes_, n_samples, p=p)
 
         # Prepare storage
         X = np.zeros((n_samples, self.n_features_))
-        X_ = np.zeros((n_samples, self.n_features_),dtype=np.int16)
+        X_ = np.zeros((n_samples, self.n_features_), dtype=np.int16)
         noise_scale = np.zeros(self.n_features_)
 
         # Generate both
         for setup in pairs:
             a, b, e = setup
             sublocation, subsample = e.get_samples(y)
-            X_[:,[a,b]] = sublocation
-            X[:,[a,b]] = subsample
-            noise_scale[[a,b]] = e.scaler_.data_range_
+            X_[:, [a, b]] = sublocation
+            X[:, [a, b]] = subsample
+            noise_scale[[a, b]] = e.scaler_.data_range_
 
         # Generate unit
         for setup in units:
             a, b, e = setup
-            partial_location = (X_[:,a], e.given_subspace[1] == a)
+            partial_location = (X_[:, a], e.given_subspace[1] == a)
             sublocation, subsample = e.get_samples(y, partial_location)
             if e.given_subspace[1] == a:
-                X_[:,[b,a]] = sublocation
-                X[:,[b,a]] = subsample
-                noise_scale[[b,a]] = e.scaler_.data_range_
+                X_[:, [b, a]] = sublocation
+                X[:, [b, a]] = subsample
+                noise_scale[[b, a]] = e.scaler_.data_range_
             else:
-                X_[:,[a,b]] = sublocation
-                X[:,[a,b]] = subsample
-                noise_scale[[a,b]] = e.scaler_.data_range_
+                X_[:, [a, b]] = sublocation
+                X[:, [a, b]] = subsample
+                noise_scale[[a, b]] = e.scaler_.data_range_
 
         noise_scale /= self.grain
-        noise = np.random.rand(X.shape[0], X.shape[1]) * noise_scale - (noise_scale/2)
+        noise = np.random.rand(X.shape[0], X.shape[1]) * noise_scale - (noise_scale / 2)
         X += noise
 
         return X, y
 
     def predict(self, X):
         # Check is fit had been called
-        check_is_fitted(self, ['X_', 'y_', 'ensemble_'])
+        check_is_fitted(self, ["X_", "y_", "ensemble_"])
 
         # Input validation
         X = check_array(X)
         if X.shape[1] != self.X_.shape[1]:
-            raise ValueError('number of features does not match')
+            raise ValueError("number of features does not match")
 
         # Establish signatures
         signatures = [clf.signatures(X) for clf in self.ensemble_]
 
         # Acquire supports
         supports = None
-        if self.fuser == 'equal':
+        if self.fuser == "equal":
             supports = np.sum(signatures, axis=0)
-        elif self.fuser == 'theta':
-            weighted_signatures = signatures * self.thetas_[:,
-                                                            np.newaxis,
-                                                            np.newaxis]
+        elif self.fuser == "theta":
+            weighted_signatures = signatures * self.thetas_[:, np.newaxis, np.newaxis]
             supports = np.sum(weighted_signatures, axis=0)
 
         # Predict
--- exposing/tests/test_exposing.py
+++ exposing/tests/test_exposing.py
@@ -9,16 +9,17 @@ from sklearn.neighbors import KNeighborsClassifier
 import timeit
 from sklearn import tree, svm, naive_bayes
 
+
 def dataset():
     n_samples = 1000
     ds = make_moons(noise=0.3, random_state=0, n_samples=n_samples)
     X, y = ds
-    return train_test_split(X, y, test_size=.4, random_state=42)
+    return train_test_split(X, y, test_size=0.4, random_state=42)
 
 
 def breast_dataset():
     X, y = load_breast_cancer(return_X_y=True)
-    return train_test_split(X, y, test_size=.4, random_state=42)
+    return train_test_split(X, y, test_size=0.4, random_state=42)
 
 
 def test_given_subspace():
@@ -27,6 +28,7 @@ def test_given_subspace():
     estimator.fit(X_train, y_train)
     score = estimator.score(X_test, y_test)
     print("\nExposer %.3f" % score)
+
 
 def test_locations_and_reverse():
     X_train, X_test, y_train, y_test = dataset()
@@ -37,6 +39,7 @@ def test_locations_and_reverse():
     locations = estimator.locations(x)
     inverse_locations = estimator.inverse_locations(locations)
 
+
 def test_rgb():
     X_train, X_test, y_train, y_test = dataset()
     estimator = Exposer()
@@ -46,58 +49,16 @@ def test_rgb():
 
 def test_ee():
     X_train, X_test, y_train, y_test = breast_dataset()
-    for fuser in ('equal', 'theta'):
+    for fuser in ("equal", "theta"):
         estimator = EE(fuser=fuser)
         estimator.fit(X_train, y_train)
         score = estimator.score(X_test, y_test)
         print("\nEE(%s) %.3f" % (fuser, score))
 
+
 def test_approaches():
     X_train, X_test, y_train, y_test = breast_dataset()
-    for approach in ('random', 'brute', 'purified'):
+    for approach in ("random", "brute", "purified"):
         estimator = EE(approach=approach)
         estimator.fit(X_train, y_train)
         score = estimator.score(X_test, y_test)
-
-def test_generation():
-    X_train, X_test, y_train, y_test = breast_dataset()
-
-    estimator = EE(approach='random', n_base=50)
-    estimator.fit(X_train, y_train)
-    a = estimator.make_classification(100)
-
-    stop = timeit.default_timer()
-
-def test_regeneration_power():
-    X_train, X_test, y_train, y_test = breast_dataset()
-
-    ee = EE(approach='random', n_base=50)
-    ee.fit(X_train, y_train)
-    X_reg, y_reg = ee.make_classification(10)
-
-    ee_score = ee.score(X_test, y_test)
-
-    clfs = {
-        'DT': tree.DecisionTreeClassifier,
-        'kNN': KNeighborsClassifier,
-        'SVC': svm.SVC,
-        'NB': naive_bayes.GaussianNB,
-        'EE': EE
-    }
-    for name in clfs:
-        clf_full = clfs[name]()
-        clf_reg = clfs[name]()
-
-        clf_full.fit(X_train, y_train)
-        clf_reg.fit(X_reg, y_reg)
-
-        score_f = clf_full.score(X_test, y_test)
-        score_r = clf_reg.score(X_test, y_test)
-
-
-def test_generation_preparing():
-    X_train, X_test, y_train, y_test = breast_dataset()
-    start = timeit.default_timer()
-
-    estimator = EE(approach='random', n_base=50)
-    estimator.fit(X_train, y_train)
